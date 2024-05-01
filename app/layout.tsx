89c732555cc7f028292750b220cb8fb545aab92c
Sairoden <odingandarosa@gmail.com>
2024-05-01 19:49:32 +0800

    Add clerk and protected routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
+// NEXT
 import type { Metadata } from "next";
+
+// STYLES
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+// LIBRARIES
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
+// LINK
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <h1 className="text-6xl font-bold text-primary">MyGPT</h1>
+          <p className="py-6 text-lg leading-loose">
+            MyGPT: Your AI language companion. Powered by OPENAI, it enhances
+            your conversations, content creation, and more!
           </p>
-          <button className="btn btn-primary">Get Started</button>
+
+          <Link href="/chat" className="btn btn-secondary">
+            Get Started
+          </Link>
         </div>
       </div>
     </div>
   );
 }
+
+// 57
