58da1d544f5c1fa022a17e283a5e209cfbd6f6a0
Sairoden <odingandarosa@gmail.com>
2024-05-01 18:30:22 +0800

    Add pages, Change HomeLayout, and Add Hero in HomePage

---
+++ app/(dashboard)/chat/page.tsx
@@ -0,0 +1,3 @@
+export default function ChatPage() {
+  return <h1 className="text-7xl">ChatPage</h1>;
+}
---
+++ app/(dashboard)/tours/page.tsx
@@ -0,0 +1,3 @@
+export default function ToursPage() {
+  return <h1 className="text-7xl">ToursPage</h1>;
+}
