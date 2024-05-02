e284b0771bbe0029db62d97811f2d032a19c9496
Sairoden <odingandarosa@gmail.com>
2024-05-02 15:37:34 +0800

    Add ThemeToggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+"use client";
+
+// REACT
+import { useState } from "react";
+
+// STYLES
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+
+const themes = {
+  winter: "winter",
+  dim: "dim",
+};
 
 export default function ThemeToggle() {
-  return <button className="btn btn-primary btn-sm">ToggleTheme</button>;
+  const [theme, setTheme] = useState(themes.dim);
+
+  const toggleTheme = () => {
+    const newTheme = theme === themes.dim ? themes.winter : themes.dim;
+    document.documentElement.setAttribute("data-theme", newTheme);
+    setTheme(newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme} className="btn btn-sm btn-outline">
+      {theme === "dim" ? (
+        <BsSunFill className="h-4 w-4" />
+      ) : (
+        <BsMoonFill className="h-4 w-4" />
+      )}
+    </button>
+  );
 }
