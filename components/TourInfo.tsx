644793880dc73cc7aa528318b941ddcdb544786f
Sairoden <odingandarosa@gmail.com>
2024-05-06 18:19:44 +0800

    Implement TourInfo

--- components/TourInfo.tsx
+++ components/TourInfo.tsx
@@ -1,9 +1,28 @@
+type Tour = {
+  title: string;
+  description: string;
+  stops: string[];
+};
+
 type TourInfoProps = {
-  tour: string;
+  tour: Tour;
 };
 
 export default function TourInfo({ tour }: TourInfoProps) {
-  console.log(tour);
+  const { title, description, stops } = tour;
 
-  return <h1>TourInfo</h1>;
+  return (
+    <div className="max-w-2xl">
+      <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+      <p className="leading-loose mb-6">{description}</p>
+
+      <ul>
+        {stops.map(stop => (
+          <li key={stop} className="mb-4 bg-base-100 p-4 rounded-xl">
+            <p>{stop}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
